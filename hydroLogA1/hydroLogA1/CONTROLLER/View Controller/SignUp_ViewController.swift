1c00fb2759a6d2fb6f7c6e0022b799217d01afdc
Benjamin Brandau <codingbrandau@gmail.com>
2020-10-09 16:24:23 -0600

    tab Bar Controller & folders

---
+++ hydroLogA1/hydroLogA1/CONTROLLER/View Controller/SignUp_ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUp_ViewController.swift
+//  hydroLogA1
+//
+//  Created by Ben Brandau Brandau on 10/9/20.
+//
+
+import UIKit
+
+class SignUp_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ hydroLogA1/hydroLogA1/MODEL/Well.swift
@@ -0,0 +1,9 @@
+//
+//  Well.swift
+//  hydroLogA1
+//
+//  Created by Ben Brandau Brandau on 10/9/20.
+//
+
+import Foundation
+
